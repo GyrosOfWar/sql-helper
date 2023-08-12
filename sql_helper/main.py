5a23a484a35cea4e59ece7825909a000cb44cd7a
Martin Tomasi <martin.tomasi@gmail.com>
2023-08-12 09:15:23 +0200

    execute query as well

--- sql_helper/main.py
+++ sql_helper/main.py
@@ -6,28 +6,7 @@ from langchain.prompts.chat import (
 from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import os
-import subprocess
-
-
-def dump_sql_schema():
-    database_name = os.environ["DATABASE_NAME"]
-    database_user = os.environ["DATABASE_USER"]
-    database_password = os.environ["DATABASE_PASSWORD"]
-    # run pg_dump process
-    output = subprocess.check_output(
-        [
-            "pg_dump",
-            "--schema-only",
-            "-x",
-            database_name,
-        ]
-    )
-    lines = output.decode("utf-8").splitlines()
-    result = []
-    for line in lines:
-        if not line.startswith("--") and line.strip() != "":
-            result.append(line)
-    return "\n".join(result)
+import psycopg
 
 
 def main():
@@ -36,7 +15,7 @@ def main():
     if not api_key:
         raise Exception("API key not found")
 
-    db_schema = dump_sql_schema()
+    db_schema = open("ddl.sql").read()
     system_template = SystemMessagePromptTemplate.from_template(
         """You are a helpful SQL assistant. Your job is to help the user write SQL queries.
            The user will provide a database schema and a natural language query. 
@@ -57,7 +36,46 @@ def main():
 
     llm = ChatOpenAI(openai_api_key=api_key)
     result = llm(template.format_messages(schema=db_schema, query=query))
-    print(result.content)
+    query = result.content
+
+    database_name = os.environ["DATABASE_NAME"]
+    database_user = os.environ["DATABASE_USER"]
+    database_password = os.environ["DATABASE_PASSWORD"]
+    db_url = f"host=localhost dbname={database_name} user={database_user} password={database_password}"
+
+    with psycopg.connect(db_url) as connection:
+        with connection.cursor() as cursor:
+            while True:
+                try:
+                    print(f"Executing query:\n{query}")
+                    cursor.execute(query)
+                    results = cursor.fetchmany(10)
+                    for row in results:
+                        print(row)
+                    print("Do you want to see all the results?")
+                    answer = input(">> ")
+                    if answer.lower() in ["yes", "y"]:
+                        results = cursor.fetchall()
+                        for row in results:
+                            print(row)
+                    break
+
+                except psycopg.ProgrammingError as e:
+                    answer = input(
+                        f"Query failed to execute with error {str(e)}, do you want to let the AI try to fix it?"
+                    )
+                    if answer.lower() in ["yes", "y"]:
+                        result = llm(
+                            template.format_messages(
+                                schema=db_schema,
+                                query="The query resulted in the following error, please fix it: "
+                                + str(e),
+                            )
+                        )
+                        query = result.content
+                        cursor = connection.cursor()
+                    else:
+                        break
 
 
 if __name__ == "__main__":
